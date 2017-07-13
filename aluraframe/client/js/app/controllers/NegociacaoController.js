5ff0b9d4401ec614de7ce1a7bd070cfee8fde81f
Stefanie Lima <sla@Stefanies-MacBook-Air.local>
2017-07-12 23:54:28 -0300

    Manipulacao da data usando map e arrow function

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,29 @@
 class NegociacaoController {
 
-    adiciona(event) {
-        event.preventDefault();
+    constructor() {
         
         let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
 
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+    adiciona(event) {
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        event.preventDefault();
+
+         let data = new Date(...
+                this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+         );
+
+         let negociacao = new Negociacao(
+             data,
+             this._inputQuantidade.value,
+             this._inputValor.value
+         );
+
+         console.log(negociacao);
     }
 }
