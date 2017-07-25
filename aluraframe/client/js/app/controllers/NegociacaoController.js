98ffbbae813e05510f89959190be9a06543df3e6
Stefanie Lima <sla@Stefanies-MacBook-Air.local>
2017-07-24 23:57:58 -0300

    Criar as views

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,33 +7,41 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+
     }
 
     adiciona(event) {
-
-        event.preventDefault();
         
+        event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._limpaFormulario();
+        this._negociacoesView.update(this._listaNegociacoes);
 
-        console.log(this._listaNegociacoes.negociacoes);
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+
+        this._limpaFormulario();   
     }
-
+    
     _criaNegociacao() {
         
-    return new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),
-        this._inputQuantidade.value,
-        this._inputValor.value);
-  }
-
-
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);    
+    }
+    
     _limpaFormulario() {
-
+     
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0
-        this._inputData.focus();
-
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
     }
 }
